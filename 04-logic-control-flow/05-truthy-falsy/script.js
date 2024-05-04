5ba260f519775c8ccd6688007b78502d41608b04
profryan <rbazur@feati.edu.ph>
2024-05-04 09:56:21 +0800

    Js Lectures

--- 04-logic-control-flow/05-truthy-falsy/script.js
+++ 04-logic-control-flow/05-truthy-falsy/script.js
@@ -23,3 +23,64 @@ if (x) {
 } else {
   console.log('This is falsy');
 }
+
+//Truthy and falsy caveats
+const children = 0;
+
+if (children) {
+  console.log(`You have ${children} children`);
+} else {
+  console.log('Please enter number of children');
+}
+
+if (!isNaN(children)) {
+  console.log(`You have ${children} children`);
+} else {
+  console.log('Please enter number of children');
+}
+
+//Checking for empty arrays
+const posts = [];
+
+//Always true even when empty
+if (posts) {
+  console.log('List Posts');
+} else {
+  console.log('No Posts to list');
+}
+
+//Solution
+if (posts.length > 0) {
+  console.log('List Posts');
+} else {
+  console.log('No Posts to list');
+}
+
+//checking for empty objects
+const user = {
+  name: 'Ryan',
+};
+
+//Always true, even when no properties
+if (user) {
+  console.log('List user');
+} else {
+  console.log('No User');
+}
+
+//Solution
+if (Object.keys(user).length > 0) {
+  console.log('List user');
+} else {
+  console.log('No User');
+}
+
+//Loose Equality
+console.log(false == 0);
+console.log('' == 0);
+console.log(null == undefined);
+
+//Strict Equality
+console.log(false === 0);
+console.log('' === 0);
+console.log(null === undefined);
